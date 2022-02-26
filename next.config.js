754fc85edb2167b196746c964b493b793570fabf
Eric Au <eric.au@berkeleylights.com>
2022-02-26 09:10:10 -0800

    redefined

--- next.config.js
+++ next.config.js
@@ -3,3 +3,10 @@ const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
 // this simply tests the redirecting of the root path (source)
 module.exports = withNextra()
+
+const basePath = process.env.NODE_ENV === 'production' ? '/edotau.github.io' : '';
+
+module.exports = {
+  basePath,
+  assetPrefix: `${basePath}/`
+};
