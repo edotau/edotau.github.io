44da2fe724cea64838ce78b4e74d3e979dad9f37
Eric Au <eric.au@berkeleylights.com>
2022-02-26 09:17:51 -0800

    redefine webapp

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
 
 // this simply tests the redirecting of the root path (source)
-module.exports = withNextra()
+
+const isProd = process.env.NODE_ENV === "production"
+
+module.exports = withNextra(),
+
+{
+  assetPrefix: isProd ? "/edotau.github.io/" : "",
+}
