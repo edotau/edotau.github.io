adad7973f4adf2446b98a6bd4a43f8b818afb91e
Eric Au <ericdotau@gmail.com>
2024-02-02 03:37:36 -0800

    revert: removed files

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: function (config) {
+    config.externals = config.externals || {};
+    config.externals["styletron-server"] = "styletron-server";
+    return config;
+  },
+  // Optionally, add any other Next.js config below
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: "export",
+  images: {
+    unoptimized: true,
+  }
+};
+
+// First apply withMDX to the nextConfig, then wrap the result with withNextra
+module.exports = withNextra(nextConfig);
---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+  logo: <span>Home</span>,
+  project: {
+    link: 'https://github.com/edotau'
+  },
+  footer: {
+    text: (
+      <span>
+        edotau et. al. © {new Date().getFullYear()} 
+      </span>
+    )
+  },
+  // ... other theme options
+}
