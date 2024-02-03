b138b377d366962abcd6828af94a67fd6d9e12ba
Eric Au <ericdotau@gmail.com>
2024-02-02 20:14:55 -0800

    style: button css position

--- pages/index.js
+++ pages/index.js
@@ -47,12 +47,32 @@ export default function Home() {
                 textSize="display1"
                 fontFamily="secondary"
                 textWeight="500"
-                p={{ x: "1rem", y: "1rem" }}
+                p={{ x: "1rem", y: "0rem" }}
               >
                 Welcome to my life
               </Text>
             </Div>
-            <Div justify="space-around" p={{ x: "1rem", y: "1rem" }} align="left">
+            </Div>
+            {
+              <Text
+              p={{ x: "1rem", y: "1rem" }}
+            >
+                My name is Eric and I'm a Software Engineer who studied Computer Science & Mathematics and knows more genetics than your average biologist. The curiosity I have for developmental biology and genomics inspired me to pursue a technical background in computer science and molecular genetics. It's incredibly motivating looking back on the knowledge I've accumulated, to realize how my areas of expertise can work together and answer questions at the intersection of computer science and biotechnology.
+              </Text>
+            }
+            {
+              <Text
+              p={{ x: "1rem", y: "1rem" }}
+            >
+                When I am not thinking about recombinant DNA and/or deployments of full stack cloud applications, I enjoy blogging about fashion and lifesyle on Pinterest. I love playing and competing in sports, especially badminton, golf, power lifting, and skiing. I cheer for the Golden State Warriors and the San Francisco 49ers and absolutely love watching my favorite NBA and NFL teams competing head to head every week.
+              </Text>
+            }
+            {
+              <Text p={{ x: "1rem", y: "1rem" }}>
+                While the best chapters of my life are being written, I'm living my best life because life's too precious to spend even a single second unhappy. I hope being who I am will start redefining the boundaries of happiness, success and hard work.
+              </Text>
+            }
+          <Div justify="space-around" p={{ x: "1rem", y: "1rem" }} align="left">
             <Anchor
               href="/experience"
               target="_self"
@@ -76,26 +96,6 @@ export default function Home() {
                 EXPERIENCE
               </Button>
             </Anchor>
-            </Div>
-            {
-              <Text
-              p={{ x: "1rem", y: "1rem" }}
-            >
-                My name is Eric and I'm a Software Engineer who studied Computer Science & Mathematics and knows more genetics than your average biologist. The curiosity I have for developmental biology and genomics inspired me to pursue a technical background in computer science and molecular genetics. It's incredibly motivating looking back on the knowledge I've accumulated, to realize how my areas of expertise can work together and answer questions at the intersection of computer science and biotechnology.
-              </Text>
-            }
-            {
-              <Text
-              p={{ x: "1rem", y: "1rem" }}
-            >
-                When I am not thinking about recombinant DNA and/or deployments of full stack cloud applications, I enjoy blogging about fashion and lifesyle on Pinterest. I love playing and competing in sports, especially badminton, golf, power lifting, and skiing. I cheer for the Golden State Warriors and the San Francisco 49ers and absolutely love watching my favorite NBA and NFL teams competing head to head every week.
-              </Text>
-            }
-            {
-              <Text p={{ x: "1rem", y: "1rem" }}>
-                While the best chapters of my life are being written, I'm living my best life because life's too precious to spend even a single second unhappy. I hope being who I am will start redefining the boundaries of happiness, success and hard work.
-              </Text>
-            }
             <Div d="flex" justify="space-around" align="center" p={{ x: "1rem", y: "2rem"}}>
               <Row>
                 <Col size={{ xs: "auto" }}>
