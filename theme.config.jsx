28c05fe0fc327ad6bbc7525b1d70a08a865e2d25
Eric Au <ericdotau@gmail.com>
2024-02-02 20:57:53 -0800

    fix: css light theme

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,6 +3,7 @@ export default {
   project: {
     link: 'https://github.com/edotau'
   },
+  themes: 'Light',
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
